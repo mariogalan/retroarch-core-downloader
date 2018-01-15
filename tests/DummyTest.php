6bb8750ee72e099b487746cbd9610a498e329455
Mario Galan <mario.galan@gmail.com>
2018-01-15 12:49:37 +0100

    Added test class

---
+++ tests/DummyTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of the project retroarch-core-downloader
+ * User: Mario Galán <mario.galan@gmail.com>
+ * Date: 15/01/18
+ * Time: 12:48
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+
+}
